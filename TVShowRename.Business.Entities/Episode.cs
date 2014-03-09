48672061ff52bcb767bf35c7006593d7ce4f5ab1
David Arrunategui <darruna@uwo.ca>
2014-03-09 16:44:32 -0400

    Added some more properties to the business entities.
    Added the bootstrapper project.

--- TVShowRename.Business.Entities/Episode.cs
+++ TVShowRename.Business.Entities/Episode.cs
@@ -8,17 +8,26 @@ namespace TVShowRename.Business.Entities
 {
     public class Episode
     {
+        public int Id { get; private set; }
+
+        public string Director { get; private set; }
+
         public string Title { get; private set; }
 
         public int Number { get; private set; }
 
         public int Season { get; private set; }
 
-        public Episode(string title, int number, int season)
+        public string Description { get; private set; }
+
+        public Episode(int id, string director, string title, int number, int season, string description)
         {
+            Id = id;
+            Director = director;
             Title = title;
             Number = number;
             Season = season;
+            Description = description;
         }
     }
 }
--- TVShowRename.Business.Entities/Show.cs
+++ TVShowRename.Business.Entities/Show.cs
@@ -10,21 +10,28 @@ namespace TVShowRename.Business.Entities
     {
         public int Id { get; private set; }
 
+        public string Language { get; private set; }
+
         public string Title { get; private set; }
 
         public string Description { get; private set; }
 
-        public string Language { get; private set; }
+        public DateTime FirstAired { get; private set; }
 
-        public string FirstAired { get; private set; }
+        public string Network { get; private set; }
 
-        public Show(int id, string title, string description, string language, string firstAired)
+        public int IMDBId { get; private set; }
+
+        public Show(int id, string title, string description, string language,
+                    DateTime firstAired, string network, int imdbId)
         {
             Id = id;
             Title = title;
             Description = description;
             Language = language;
             FirstAired = firstAired;
+            Network = network;
+            IMDBId = imdbId;
         }
     }
 }
