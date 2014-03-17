19f10b4bb0db020970ee8e070b89e65640d8a19b
darrunategui <darrunategui@phoenix-interactive.com>
2014-03-17 13:36:30 -0400

    Added a TVShowFile class as a business entity

---
+++ TVShowRename.Business.Entities/TVShowFile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVShowRename.Business.Entities
+{
+   public class TVShowFile
+   {
+      public string Filename { get; set; }
+
+      public string Show { get; set; }
+
+      public int Season { get; set; }
+
+      public int Episode { get; set; }
+
+      public TVShowFile(string filename, string title, int season, int episode)
+      {
+         Filename = filename;
+         Show = title;
+         Season = season;
+         Episode = episode;
+      }
+   }
+}
