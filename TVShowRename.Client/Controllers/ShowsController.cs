96ad15726454f754e0891274f8fbba489050042b
darrunategui <darrunategui@phoenix-interactive.com>
2014-05-02 12:09:42 -0400

    Added constructor to be used when a view is not necessary.

--- TVShowRename.Client/Controllers/ShowsController.cs
+++ TVShowRename.Client/Controllers/ShowsController.cs
@@ -29,9 +29,11 @@ namespace TVShowRename.Client.Controllers
       private IEnumerable<Show> _shows;
       #endregion
 
+      #region Constructors
       /// <summary>
       /// Initializes a new instance of the ShowsController object.
       /// </summary>
+      /// <remarks>Should only be used when a view is to be shown to display multiple show results.</remarks>
       /// <param name="view">The associated view for this controller.</param>
       /// <param name="fileToRename">The file to rename.</param>
       /// <param name="shows">The show results to display on the view.</param>
@@ -42,6 +44,17 @@ namespace TVShowRename.Client.Controllers
          _fileToRename = fileToRename;
          _shows = shows;
       }
+
+      /// <summary>
+      /// Initializes a new instance of the ShowsController object.
+      /// </summary>
+      /// <remarks>Should only be used when a view is to be shown to display multiple show results.</remarks>
+      /// <param name="filetoRename">The file to rename.</param>
+      public ShowsController(TVShowFile filetoRename)
+      {
+         _fileToRename = filetoRename;
+      }
+      #endregion
 
       /// <summary>
       /// Initializes the view with possible show results.
@@ -68,7 +81,7 @@ namespace TVShowRename.Client.Controllers
                                where e.Season == _fileToRename.SeasonNumber &&
                                      e.Number == _fileToRename.EpisodeNumber
                                select e).FirstOrDefault();
-            if ( episode == null )
+            if (episode == null)
             {
                // TODO: should be some way to determine if it was successfull or not.
                // Maybe just use First() and catch the exception if nothing is found.
@@ -76,8 +89,9 @@ namespace TVShowRename.Client.Controllers
             }
 
             // TODO: status - building the new filename.
+            // TODO: Allow multiple output filename templates
             string newFilename = String.Format("{0} S{1}E{2} - {3}", show.Title, episode.SeasonAsString(), episode.NumberAsString(), episode.Title);
-            
+
             // Strip off any illegal filename characters.
             string invalidChars = new string(Path.GetInvalidFileNameChars());
             Regex regex = new Regex(String.Format("[{0}]", Regex.Escape(invalidChars)));
@@ -86,11 +100,22 @@ namespace TVShowRename.Client.Controllers
             // TODO: status - renaming file.
             File.Move(_fileToRename.Filename, String.Format(@"{0}\{1}", Directory.GetParent(_fileToRename.Filename), newFilename));
             // TODO: I want to know if any exception occurs. Log it out or even just show a message box for testing.
+            // The previous version of TVShow rename got hung up way to often without any indication of what was happening.
          }
-         catch ( Exception ex )
+         catch (Exception ex)
          {
             MessageBox.Show(ex.Message);
          }
+      }
+
+      private void UpdateStatus(string status)
+      {
+         // TODO: check if the status should be updated (is there a view to update or not).
+         if ( _view == null )
+         {
+            return;
+         }
+         // Continue..
       }
 
    }
