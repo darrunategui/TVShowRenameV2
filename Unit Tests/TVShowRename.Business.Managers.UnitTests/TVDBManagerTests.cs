4c01baa9e0ef155fb41a6d7dba39f1ca82484f84
darrunategui <darrunategui@phoenix-interactive.com>
2014-05-14 16:09:08 -0400

    Added TVDBManager test.

---
+++ Unit Tests/TVShowRename.Business.Managers.UnitTests/TVDBManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using TVShowRename.Business.Entities;
+using System.Threading.Tasks;
+using System.Net;
+using TVShowRename.Common;
+
+namespace TVShowRename.Business.Managers.UnitTests
+{
+   [TestClass]
+   public class TVDBManagerTests
+   {
+      TVDBManager _tvdbManager;
+
+      [TestInitialize]
+      public void Initialize()
+      {
+         _tvdbManager = new TVDBManager();
+      }
+
+      [TestMethod]
+      [ExpectedException(typeof(ShowNotFoundException))]
+      public async Task GetShowsByTitle_UnknownTitle_throws()
+      {
+         try
+         {
+            List<Show> shows = await _tvdbManager.GetShowsByTitle("hjbijbkj");
+         }
+         catch (Exception)
+         {
+            throw;
+         }
+      }
+   }
+}
